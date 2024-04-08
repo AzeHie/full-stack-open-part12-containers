955f74a483c7611e030341213c8b91e97498de39
AZE <azehietala@gmail.com>
2024-04-08 15:56:13 +0300

    Mongo db configuration

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose')
 const Todo = require('./models/Todo')
 const { MONGO_URL } = require('../util/config')
 
-if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log(MONGO_URL);
+
+if (MONGO_URL) {
+  mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log('Connected to MongoDB database');
+    }).catch(error => {
+      console.error('Error connecting to MongoDB:', error);
+    });
+}
 
 
 module.exports = {
