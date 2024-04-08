9020132a6b04591eb2e7188707cf161dde82212c
AZE <azehietala@gmail.com>
2024-04-08 16:19:19 +0300

    Finished exercise 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { Todo } = require('../mongo')
+const { Todo } = require('../mongo');
 const router = express.Router();
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
+  const todos = await Todo.find({});
   res.send(todos);
 });
 
@@ -12,38 +12,60 @@ router.get('/', async (_, res) => {
 router.post('/', async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
-  })
+    done: false,
+  });
   res.send(todo);
 });
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
+  const { id } = req.params;
 
-  next()
-}
+  try {
+    req.todo = await Todo.findById(id);
+    if (!req.todo) return res.sendStatus(404);
+  } catch (err) {
+    console.log(
+      'Failed to find todo by specified ID, something went wrong',
+      err
+    );
+    res
+      .status(500)
+      .json('Failed to find todo by specified ID, something went wrong');
+  }
+
+  next();
+};
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+  await req.todo.delete();
   res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  try {
+    const id = req.params;
+    const updatedTodo = await Todo.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    if (!updatedTodo) {
+      return res.status(404).json('Failed to update todo');
+    }
+  } catch (err) {
+    console.error('Failed to update todo: ', err);
+    res.status(500).json('Failed to update todo');
+  }
 });
 
-router.use('/:id', findByIdMiddleware, singleRouter)
-
+router.use('/:id', findByIdMiddleware, singleRouter);
 
 module.exports = router;
